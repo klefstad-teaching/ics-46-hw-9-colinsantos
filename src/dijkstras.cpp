80facb4086ab2fbda8a8dd05f7fe46d943ae25c4
Colin Santos <colsantos11@gmail.com>
2025-03-16 20:19:29 +0000

    Incremental commit

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,15 +1,18 @@
 #include "dijkstras.h"
+#include <algorithm>
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
     int numVertices = G.size();
     vector<int> distances(numVertices, INF);
     vector<bool> visited(numVertices, false);
     distances[source] = 0;
+    previous.assign(numVertices, -1);
     
-    priority_queue<pair<int,int>> minHeap;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
     minHeap.push({source, 0});
     while (!minHeap.empty()) {
         int u = minHeap.top().first;
+        minHeap.pop();
 
         if (visited[u]) {
             continue;
@@ -28,4 +31,31 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
             }
         }
     }
+
+    return distances;
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
+    std::vector<int> shortest_path;
+
+    if (previous[destination] == -1 && destination != 0) {
+        return shortest_path;
+    }
+
+    for (int v = destination; v != -1; v = previous[v]) {
+        shortest_path.push_back(v);
+    }
+
+    std::reverse(shortest_path.begin(), shortest_path.end());
+
+    return shortest_path;
+}
+
+void print_path(const vector<int>& v, int total) {
+    for (int i : v) {
+        std::cout << i << " ";
+    }
+    std::cout << endl;
+
+    std::cout << "Total: " << total << endl;
 }
